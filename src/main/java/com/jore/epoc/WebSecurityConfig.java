f1a8062a9e5fba284ddc3308670b13b829136016
Thomas Süssli <thomassussli@apoc-air.home>
2023-05-21 10:23:03 +0200

    Minor changes

--- src/main/java/com/jore/epoc/WebSecurityConfig.java
+++ src/main/java/com/jore/epoc/WebSecurityConfig.java
@@ -6,11 +6,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-import lombok.extern.log4j.Log4j2;
-
 @Configuration
 @EnableWebSecurity
-@Log4j2
 public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
